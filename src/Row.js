ff8d7e6416973a0c24f1c50193c44223511f1a56
Ankit <ankitchadda285@gmail.com>
2021-03-09 10:59:28 +0530

    Almost Done

---
+++ src/Row.js
@@ -0,0 +1,72 @@
+import React, { useEffect, useState } from "react";
+import axios from "./axios";
+import "./Row.css";
+import Youtube from "react-youtube";
+import movieTrailer from "movie-trailer";
+
+const base_Url = "https://image.tmdb.org/t/p/original/";
+
+const Row = (props) => {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(props.fetchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [props.fetchUrl]);
+
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  };
+
+  const handleClick = (movie) => {
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      console.log(movie);
+      movieTrailer(movie?.title || movie?.original_name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          console.log(url);
+
+          setTrailerUrl(urlParams.get("v"));
+        })
+        .catch((error) => console.log(error));
+    }
+  };
+
+  return (
+    <>
+      <div className="row_div">
+        <h2>{props.title}</h2>
+
+        <div className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              onClick={() => handleClick(movie)}
+              className={`row__poster ${
+                props.isLargeRow && "row__posterLarge"
+              }`}
+              src={`${base_Url}${
+                props.isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+      </div>
+    </>
+  );
+};
+export default Row;
---
+++ src/request.jsx
@@ -0,0 +1,14 @@
+const API_KEY = "fa21901c6afb8e82086f44bd1d6df210";
+
+const requests = {
+    fetchTrending : `/trending/all/week?api_key=${API_KEY}&language=en-US`,
+    fetchNetflixOriginals : `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`
+}
+
+export default requests;
